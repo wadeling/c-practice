6d8b64a0698843c2533138a21eada2917fcf9bb8
wadeling <wadeling@gmail.com>
2019-11-08 11:06:45 +0800

    add benchtest

--- llhttp-demo/src/main.c
+++ llhttp-demo/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include "llhttp.h"
 
 int handle_on_message_begin(llhttp_t* type) {
@@ -16,38 +17,48 @@ int main(int argc, char** argv) {
 
     /* Set user callback */
 //    settings.on_message_complete = handle_on_message_complete;
-    settings.on_message_begin = handle_on_message_begin;
+//    settings.on_message_begin = handle_on_message_begin;
 
     /* Initialize the parser in HTTP_BOTH mode, meaning that it will select between
      * HTTP_REQUEST and HTTP_RESPONSE parsing automatically while reading the first
      * input.
      */
-    llhttp_init(&parser, HTTP_REQUEST, &settings);
+    llhttp_init(&parser, HTTP_BOTH, &settings);
 
     /* Parse request! */
-    const char* request = "GET / HTTP/1.1\r\n\r\n";
+    const char* request = "GET / HTTP/1.1\r\n"
+                         "Host: 127.0.0.1:9211\r\n"
+                         "User-Agent: fortio.org/fortio-1.3.2-pre\r\n"
+                         "X-Id: 1\r\n";
     int request_len = strlen(request);
 
-//    llhttp_pause(&parser); // will pause excute
-    
-    enum llhttp_errno err = llhttp_execute(&parser, request, request_len);
-    if (err == HPE_OK) {
-        /* Successfully parsed! */
-        printf("parsed ok.\r\n");
-    } else {
-        fprintf(stderr, "Parse error: %s %s\n", llhttp_errno_name(err),
-                parser.reason);
+    time_t start,end;
+    start = clock();
+    int num = 1 << 10;
+    printf("test num %d\r\n",num);
+    for (int i = 0; i < num; ++i) {
+        llhttp_init(&parser, HTTP_BOTH, &settings);
+        enum llhttp_errno err = llhttp_execute(&parser, request, request_len);
+        if (err == HPE_OK) {
+            /* Successfully parsed! */
+//            printf("parsed ok.\r\n");
+        } else {
+            fprintf(stderr, "%d Parse error: %s %s\n",i, llhttp_errno_name(err),
+                    parser.reason);
+            const char* error_pos = llhttp_get_error_pos(&parser);
+            if (error_pos != NULL) {
+                printf("get err pos,%s \r\n",error_pos);
+            } else {
+                printf("no err pos \r\n");
+            }
+            break;
+        }
     }
 
-
-    printf(" llhttp_get_errno(&parser) %d\r\n",llhttp_get_errno(&parser));
-
-    const char* error_pos = llhttp_get_error_pos(&parser);
-    if (error_pos != NULL) {
-        printf("get err pos \r\n");
-    } else {
-        printf("no err pos \r\n");
-    }
+    end = clock();
+    double total_time = difftime(end,start);
+    double parsed_time = total_time/(double)(num);
+    printf("take time %f ms, every parse action take time %f ms\r\n",total_time,parsed_time);
 
 	printf("end");
 
