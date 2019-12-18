a2936a61f97af42bf391d3e12bfa66e13852bfc1
wadeling <wadeling@gmail.com>
2019-12-18 17:21:40 +0800

    add que shm

---
+++ que_shm_demo/src/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <chrono>
+#include <time.h>
+#include <sys/time.h>
+#include "que_shm.h"
+
+using namespace std;
+
+
+int main() {
+    int result;
+    std::string value="aaa";
+    CQueShm test_que("test_que",1000,10,10);
+    result = test_que.Put(value);
+
+    std::string value2;
+    int pop_result;
+    pop_result = test_que.Pop(value2);
+ 
+	printf("pop %s\r\n",pop_result.c_str());
+    printf("end\r\n");
+    return 0;
+}
